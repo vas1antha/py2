ccbd6ca5d018b0e6fd1830bd11746da3753d3f65
vas1antha <133865319+vas1antha@users.noreply.github.com>
2023-11-07 21:53:42 -0800

    Add files via upload

---
+++ python1.py
@@ -0,0 +1,16 @@
+print("Hi,Welcome to python progamining")
+print("Hi,Welcome to 3rd sem computer science class")
+
+a=100
+b=300
+print("The Sum is ",a+b)
+c=a+b
+print("The Sum is",c)
+print("The Sum of",a,"and",b,"is",c)
+print("The Sum of a and b is",c)
+x=45
+y=58
+z=63
+print("The value of x is",x)
+print("The value of y is",z)
+print("The value of x is",x,"zand the value of y is",y,"and the value of z is",z)
